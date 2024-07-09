dc1619462405b0e23c5db79b004669232cb715e0
unknown <nightfury1904@gmail.com>
2024-07-09 16:11:15 +0700

    merge code BE

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,25 +1,45 @@
 // import library 
 const express = require('express');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const morgan = require("morgan"); 
-const { fetchDataAndSaveToMongo } = require('./utils/apiUtils');
 const path = require('path');
-// import structure
-const config = require('./config/mongodb');  
-const port = config.port;
-const app = express()
-app.use(express.static(path.join(__dirname,'public')))// xử lý static file nằm trong public 
-// req HTTP  
-app.use(morgan('combined'))
+const { fetchDataAndSaveToMongo } = require('./utils/apiUtils');
+
+// Configure environment variables
+dotenv.config();
+
+// Create Express app
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middleware setup
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-// import routes 
+
+// Import and setup routes
 const userRoutes = require('./routes/appRoutes');
-// Routes init
 userRoutes(app);
-// start server
+
+// Connect to MongoDB
+mongoose.connect(`${process.env.MONGO_DB}`)
+    .then(() => {
+        console.log('Connect database successful!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// Start the server
 app.listen(port, async () => {
-  console.log(`Server đang chạy tại http://localhost:${port}`);
-  // Fetch and save data to MongoDB when server starts
-  await fetchDataAndSaveToMongo();
+    console.log(`Server is running at http://localhost:${port}`);
+    // Fetch and save data to MongoDB when server starts
+    await fetchDataAndSaveToMongo();
 });
-
-
---
+++ BackEnd/src/services/UserService.js
@@ -0,0 +1,186 @@
+const User = require("../models/UserModel")
+const bcrypt = require("bcrypt")
+const { generalAccessToken, generalRefreshToken } = require("./JwtService")
+
+const createUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { username, email, password } = newUser
+        try {
+            const checkEmail = await User.findOne({
+                email: email
+            })
+            if (checkEmail) {
+                return resolve({
+                    status: 'ERR',
+                    message: 'The email is already existed'
+                });
+            }
+
+            const checkUser = await User.findOne({
+                username: username
+            })
+            if(checkUser !== null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The username is already existed'
+                })
+            }
+
+            const hash = bcrypt.hashSync(password, 10);
+            const createdUser = await User.create({
+                email, 
+                username, 
+                password: hash, 
+            })
+            if(createdUser) {
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: createdUser
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const loginUser = (userLogin) => {
+    return new Promise(async (resolve, reject) => {
+        const { username, password } = userLogin
+        try {
+            const checkUser = await User.findOne({
+                username: username
+            })
+            if(checkUser === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The user is not defined'
+                })
+            }
+            const comparePassword = bcrypt.compareSync(password, checkUser.password)
+
+            if(!comparePassword) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The password or user is incorrect'
+                })
+            }
+            const access_token = await generalAccessToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+
+            const refresh_token = await generalRefreshToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                access_token,
+                refresh_token
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const updateUser = (id, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({
+                _id: id
+            })
+            if(checkUser === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined'
+                })
+            }
+
+            const updatedUser = await User.findByIdAndUpdate(id, data, { new: true })
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: updatedUser
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const deleteUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({
+                _id: id
+            })
+            if(checkUser === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined'
+                })
+            }
+
+            await User.findByIdAndDelete(id)
+            resolve({
+                status: 'OK',
+                message: 'Delete user successful',
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const getAllUser = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const allUser = await User.find()
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: allUser
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const getDetailsUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findOne({
+                _id: id
+            })
+            if(user === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined'
+                })
+            }
+
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: user
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+module.exports = {
+    createUser,
+    loginUser,
+    updateUser,
+    deleteUser,
+    getAllUser,
+    getDetailsUser
+}
